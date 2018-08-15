beab0066f151ba9362190642ec60fd40811d835d
Fan Yinghao <fanyinghao@gmail.com>
2018-08-15 21:55:59 +0800

    修改eosjs init

--- app/client/lib/eos/init.js
+++ app/client/lib/eos/init.js
@@ -1,6 +1,8 @@
 Eos = require('eosjs')
 const {observableAccounts} = require('./observableAccounts')
 
+httpEndpoint = 'http://192.168.2.192:10053';
+
 chain = {
     mainnet: 'aca376f206b8fc25a6ed44dbdc66547c36c6c33e3a119ffbeaef943642f0e906',
     testnet: '038f4b0fc8ff18a4f0842a8f0564611f6e96e8535901dd45e43ac8691a1c4dca',
@@ -8,7 +10,7 @@ chain = {
 }
 
 eos = Eos({
-    httpEndpoint: 'http://192.168.2.192:10053',
+    httpEndpoint: httpEndpoint,
     chainId: chain.testnet,
     verbose: false
 })
