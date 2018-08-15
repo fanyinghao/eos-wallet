def8d7bc8f1c6bba17d12fecaebc0d4f744a873c
Fan Yinghao <fanyinghao@gmail.com>
2018-08-15 21:56:20 +0800

    add keystore

---
+++ app/client/lib/eos/keystore.js
@@ -0,0 +1,25 @@
+import { SecureStorage } from "../../lib/eosjs-SecureStorage/lib";
+
+const STORGE_ID = "EOS_ACCOUNT";
+
+export function SetKey(accountName, password, privateKey, publicKey) {
+  if (!publicKey) publicKey = ecc.privateToPublic(privateKey);
+  // storage private key
+  const storage = new SecureStorage({ id: STORGE_ID });
+  storage.set(accountName, { publicKey, sensitive: { privateKey } }, password);
+}
+
+export function SignProvider(accountName, password) {
+  const storage = new SecureStorage({ id: STORGE_ID });
+  let sensitive = storage.get(accountName, password).sensitive;
+  if(!sensitive) 
+    throw new Error('wrong password');
+  let provider = ({sign, buf}) => sign(buf, sensitive.privateKey);
+  return provider;
+}
+
+export function KeyProvider(accountName, password) {
+    const storage = new SecureStorage({ id: STORGE_ID });
+    let wif = storage.get(accountName, password).sensitive.privateKey;
+    return [wif];
+}
