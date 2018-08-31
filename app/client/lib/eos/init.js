fbc927619db65cbbd9598b2a23394de36e20279a
Fan Yinghao <fanyinghao@gmail.com>
2018-08-31 18:42:17 +0800

    更新节点

--- app/client/lib/eos/init.js
+++ app/client/lib/eos/init.js
@@ -4,7 +4,7 @@ ObservableAccounts = observableAccounts;
 
 chains = {
   zbeos: {
-    httpEndpoint: 'https://node1.zbeos.com',
+    httpEndpoint: 'https://n4.zb.cn',
     chainId: 'aca376f206b8fc25a6ed44dbdc66547c36c6c33e3a119ffbeaef943642f0e906',
     transactionMonitor: 'https://eosmonitor.io/txns'
   },
