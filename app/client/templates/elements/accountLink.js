5c7ea17e8c02b6cdd60af6580e6e0b05c6700649
Fan Yinghao <fanyinghao@gmail.com>
2018-09-01 14:33:54 +0800

    删除无用代码

--- app/client/templates/elements/accountLink.js
+++ app/client/templates/elements/accountLink.js
@@ -13,51 +13,5 @@ Template['elements_account_link'].helpers({
     */
   getAccount: function() {
     return {name: this.name};
-  },
-  /**
-    Adds class about ens
-
-    @method (ensClass)
-    */
-  ensClass: function() {
-    return this.ens ? 'ens-name' : 'not-ens-name';
-  },
-  /**
-    Displays ENS names with triangles
-
-    @method (nameDisplay)
-    */
-  displayName: function() {
-    return this.ens
-      ? this.name
-          .split('.')
-          .slice(0, -1)
-          .reverse()
-          .join(' ▸ ')
-      : this.name;
-  },
-  /**
-    Displays ENS names with triangles
-
-    @method (nameDisplay)
-    */
-  tryENS: function() {
-    var template = Template;
-    var _this = this;
-
-    Helpers.getENSName(this.address, function(err, name, returnedAddr) {
-      if (err) {
-        console.log(err);
-        return;
-      }
-
-      if (this.address && returnedAddr) {
-        if (this.address.toLowerCase() === returnedAddr.toLowerCase()) {
-          console.log('ens', name, _this, template);
-          // _this.name = name;
-          // TemplateVar.set(template, 'ensName', name)
-        }
-      }
-    });
   }
 });
