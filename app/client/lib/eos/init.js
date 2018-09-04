dc2734885aba658d1f7a5324d2e627aa1579cc6d
Fan Yinghao <fanyinghao@gmail.com>
2018-09-04 20:54:26 +0800

    更新node

--- app/client/lib/eos/init.js
+++ app/client/lib/eos/init.js
@@ -9,7 +9,7 @@ chains = {
     transactionMonitor: 'https://eosmonitor.io/txns'
   },
   'zbeos.kylin': {
-    httpEndpoint: 'http://192.168.2.192:10053',
+    httpEndpoint: 'https://testnet.zbeos.com',
     chainId: '5fff1dae8dc8e2fc4d5b23b2c7665c97f9e9d8edf2b6485a86ba311c25639191',
     transactionMonitor: 'https://tools.cryptokylin.io/#/tx'
   },
