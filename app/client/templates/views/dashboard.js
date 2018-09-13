f3d9fcafb06a5b0c8cbd1f9261bb7e9152df45bf
Fan Yinghao <fanyinghao@gmail.com>
2018-09-13 17:50:15 +0800

    [优化]未完成创建账户可以删除，加入删除按钮

--- app/client/templates/views/dashboard.js
+++ app/client/templates/views/dashboard.js
@@ -1,3 +1,4 @@
+const keystore = require("../../lib/eos/keystore");
 /**
 Template Controllers
 
@@ -10,7 +11,7 @@ The dashboard template
 @class [template] views_dashboard
 @constructor
 */
-var reactive_node = new ReactiveVar(localStorage.getItem('chain_node'));
+var reactive_node = new ReactiveVar(localStorage.getItem("chain_node"));
 var reactiveAccounts = new ReactiveVar([]);
 
 Template.views_dashboard.created = function() {
@@ -21,11 +22,11 @@ Template.views_dashboard.created = function() {
     for (var i = 0; i < localStorage.length; i++) {
       let key = localStorage.key(i);
 
-      if (key.indexOf('EOS_ACCOUNT') >= 0) {
-        let cid = key.substring(12).split('_')[0];
+      if (key.indexOf("EOS_ACCOUNT") >= 0) {
+        let cid = key.substring(12).split("_")[0];
 
         if (cid === chains[_node].chainId) {
-          let name = key.substring(12).split('_')[1];
+          let name = key.substring(12).split("_")[1];
           let account = {
             name: name,
             publicKey: localStorage[key].publicKey,
@@ -40,7 +41,7 @@ Template.views_dashboard.created = function() {
   });
 };
 
-Template['views_dashboard'].helpers({
+Template["views_dashboard"].helpers({
   /**
     Get all current accounts
 
@@ -63,7 +64,7 @@ Template['views_dashboard'].helpers({
       if (key.indexOf(`EOS_ACCOUNT_${chains[_node].chainId}`) >= 0) return true;
     }
     reactive_node.set(_node);
-    console.log('recompute hasAccounts');
+    console.log("recompute hasAccounts");
 
     return false;
   },
@@ -88,7 +89,7 @@ Template['views_dashboard'].helpers({
     return Object.keys(chains);
   },
   selected: function(value) {
-    let ret = value === localStorage.getItem('chain_node') ? 'selected' : '';
+    let ret = value === localStorage.getItem("chain_node") ? "selected" : "";
     return ret;
   },
   version: function() {
@@ -96,21 +97,31 @@ Template['views_dashboard'].helpers({
   }
 });
 
-Template['views_dashboard'].events({
+Template["views_dashboard"].events({
   /**
     Request to create an account in mist
 
     @event click .create.account
     */
-  'click .create.account': function(e) {
+  "click .create.account": function(e) {
     e.preventDefault();
   },
 
-  'change select[name=chain_node]': function(e, template) {
+  "change select[name=chain_node]": function(e, template) {
     reload_chain(e.target.value);
     reactive_node.set(e.target.value);
     reactiveAccounts.set([]);
 
-    console.log('clicked');
+    console.log("clicked");
+  },
+  "click .wallet-box-remove": function(e) {
+    let accounts = reactiveAccounts.get();
+    let name = e.currentTarget.dataset.name;
+    keystore.Remove(name);
+    reactiveAccounts.set(
+      accounts.filter(e => {
+        return e.name !== name;
+      })
+    );
   }
 });
