e36be207411b94fd2baf76356e393f5d26fc3215
Fan Yinghao <fanyinghao@gmail.com>
2018-09-13 14:03:34 +0800

    修改文本

--- app/client/lib/eos/init.js
+++ app/client/lib/eos/init.js
@@ -8,7 +8,7 @@ chains = {
     chainId: 'aca376f206b8fc25a6ed44dbdc66547c36c6c33e3a119ffbeaef943642f0e906',
     transactionMonitor: 'https://eosmonitor.io/txns'
   },
-  'zbeos.kylin': {
+  'zbeos测试': {
     httpEndpoint: 'https://testnet.zbeos.com',
     chainId: '5fff1dae8dc8e2fc4d5b23b2c7665c97f9e9d8edf2b6485a86ba311c25639191',
     transactionMonitor: 'https://tools.cryptokylin.io/#/tx'
@@ -18,7 +18,7 @@ chains = {
     chainId: 'aca376f206b8fc25a6ed44dbdc66547c36c6c33e3a119ffbeaef943642f0e906',
     transactionMonitor: 'https://www.myeoskit.com/#/tx'
   },
-  'eosasia.kylin': {
+  'eosasia测试': {
     httpEndpoint: 'https://api-kylin.eosasia.one',
     chainId: '5fff1dae8dc8e2fc4d5b23b2c7665c97f9e9d8edf2b6485a86ba311c25639191',
     transactionMonitor: 'https://tools.cryptokylin.io/#/tx'
