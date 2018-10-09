70ea265302626694f75e4216707ed5b608cd38f7
Fan Yinghao <fanyinghao@gmail.com>
2018-10-09 17:20:14 +0800

    更新节点api

--- app/client/lib/eos/init.js
+++ app/client/lib/eos/init.js
@@ -1,38 +1,38 @@
-Eos = require('eosjs');
-const { observableAccounts } = require('./observableAccounts');
+Eos = require("eosjs");
+const { observableAccounts } = require("./observableAccounts");
 ObservableAccounts = observableAccounts;
 
 chains = {
   zbeos: {
-    httpEndpoint: 'https://n4.zb.cn',
-    chainId: 'aca376f206b8fc25a6ed44dbdc66547c36c6c33e3a119ffbeaef943642f0e906',
-    transactionMonitor: 'https://eosmonitor.io/txns'
+    httpEndpoint: "https://n4.zb.cn",
+    chainId: "aca376f206b8fc25a6ed44dbdc66547c36c6c33e3a119ffbeaef943642f0e906",
+    transactionMonitor: "https://eospark.com/MainNet/tx"
   },
-  'zbeos测试': {
-    httpEndpoint: 'https://testnet.zbeos.com',
-    chainId: '5fff1dae8dc8e2fc4d5b23b2c7665c97f9e9d8edf2b6485a86ba311c25639191',
-    transactionMonitor: 'https://tools.cryptokylin.io/#/tx'
+  zbeos测试: {
+    httpEndpoint: "http://47.52.114.6:9999",
+    chainId: "5fff1dae8dc8e2fc4d5b23b2c7665c97f9e9d8edf2b6485a86ba311c25639191",
+    transactionMonitor: "https://tools.cryptokylin.io/#/tx"
   },
   eosasia: {
-    httpEndpoint: 'https://api1.eosasia.one',
-    chainId: 'aca376f206b8fc25a6ed44dbdc66547c36c6c33e3a119ffbeaef943642f0e906',
-    transactionMonitor: 'https://www.myeoskit.com/#/tx'
+    httpEndpoint: "https://geo.eosasia.one",
+    chainId: "aca376f206b8fc25a6ed44dbdc66547c36c6c33e3a119ffbeaef943642f0e906",
+    transactionMonitor: "https://www.myeoskit.com/tx"
   },
-  'eosasia测试': {
-    httpEndpoint: 'https://api-kylin.eosasia.one',
-    chainId: '5fff1dae8dc8e2fc4d5b23b2c7665c97f9e9d8edf2b6485a86ba311c25639191',
-    transactionMonitor: 'https://tools.cryptokylin.io/#/tx'
+  eosasia测试: {
+    httpEndpoint: "https://api-kylin.eosasia.one",
+    chainId: "5fff1dae8dc8e2fc4d5b23b2c7665c97f9e9d8edf2b6485a86ba311c25639191",
+    transactionMonitor: "https://tools.cryptokylin.io/#/tx"
   }
 };
 
 eos = null;
-httpEndpoint = '';
-transactionMonitor = '';
-chainId = '';
-var chain_node = localStorage.getItem('chain_node');
+httpEndpoint = "";
+transactionMonitor = "";
+chainId = "";
+var chain_node = localStorage.getItem("chain_node");
 
 reload_chain = function(_node) {
-  localStorage.setItem('chain_node', _node);
+  localStorage.setItem("chain_node", _node);
   let node = chains[_node];
 
   httpEndpoint = node.httpEndpoint;
@@ -49,7 +49,7 @@ reload_chain = function(_node) {
 };
 
 if (!chain_node) {
-  localStorage.setItem('chain_node', 'zbeos');
-  chain_node = 'zbeos';
+  localStorage.setItem("chain_node", "zbeos");
+  chain_node = "zbeos";
 }
 reload_chain(chain_node);
