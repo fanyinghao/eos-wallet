4993bd5d8e88c3a30d60dc7bab378f6402523790
fanyinghao <fanyinghao@gmail.com>
2018-11-21 17:39:41 +0800

    自定义节点

--- app/client/templates/views/settings.js
+++ app/client/templates/views/settings.js
@@ -1,6 +1,6 @@
-const keystore = require("../../lib/eos/keystore");
-const http = require("../../lib/helpers/http");
-Eos = require("eosjs");
+const keystore = require('../../lib/eos/keystore');
+const http = require('../../lib/helpers/http');
+Eos = require('eosjs');
 /**
 Template Controllers
 
@@ -15,16 +15,20 @@ The add user template
 */
 
 // Set basic variables
-Template["views_settings"].onCreated(function() {});
+Template['views_settings'].onCreated(function() {});
 
 var reactive_nodes = new ReactiveVar({});
+var reactive_custom_nodes = new ReactiveVar(
+  JSON.parse(localStorage.getItem('custom_nodes'))
+);
 var reactive_cur_node = new ReactiveVar(
-  JSON.parse(localStorage.getItem("cur_node"))
+  JSON.parse(localStorage.getItem('cur_node'))
 );
 
 Template.views_settings.onRendered(function() {
   var template = this;
   var nodes = reactive_nodes.get();
+  var custom_nodes = reactive_custom_nodes.get();
 
   Tracker.autorun(c => {
     Helpers.getProducers().then(data => {
@@ -32,12 +36,24 @@ Template.views_settings.onRendered(function() {
         map[item.owner] = item;
         return map;
       }, {});
-      reactive_nodes.set(nodes);
+
+      if (custom_nodes)
+        Array.prototype.forEach.call(custom_nodes, (item, index, arr) => {
+          http
+            .ping({
+              url: item.node + '/v1/history/get_actions',
+              method: 'post'
+            })
+            .then(tls => {
+              arr[index].tls = tls;
+              reactive_custom_nodes.set(arr);
+            });
+        });
 
       Array.prototype.forEach.call(data, item => {
         http
           .get({
-            url: item.url + "/bp.json"
+            url: item.url + '/bp.json'
           })
           .then(
             res => {
@@ -53,8 +69,8 @@ Template.views_settings.onRendered(function() {
                   : node.api_endpoint;
                 http
                   .ping({
-                    url: url + "/v1/history/get_actions",
-                    method: "post"
+                    url: url + '/v1/history/get_actions',
+                    method: 'post'
                   })
                   .then(tls => {
                     nodes[item.owner].tls = tls;
@@ -64,7 +80,7 @@ Template.views_settings.onRendered(function() {
               });
             },
             err => {
-              console.log(err);
+              console.error(err);
             }
           );
       });
@@ -72,10 +88,13 @@ Template.views_settings.onRendered(function() {
   });
 });
 
-Template["views_settings"].helpers({
+Template['views_settings'].helpers({
   nodes: function() {
     let nodes = reactive_nodes.get();
-    return Object.values(nodes);
+    let custom_nodes = reactive_custom_nodes.get();
+    if (!custom_nodes) custom_nodes = [];
+
+    return custom_nodes.concat(Object.values(nodes));
   },
   curNode: function() {
     let cur_node = reactive_cur_node.get();
@@ -83,15 +102,45 @@ Template["views_settings"].helpers({
   }
 });
 
-Template["views_settings"].events({
-  "click tr": function(e) {
-    console.log("click", this);
+Template['views_settings'].events({
+  'click tr': function(e) {
     if (!this.node) return;
     reactive_cur_node.set({ owner: this.owner, node: this.node });
     localStorage.setItem(
-      "cur_node",
+      'cur_node',
       JSON.stringify({ owner: this.owner, node: this.node })
     );
     reload_chain(this.node);
+  },
+  'keyup input[name="addNode"], change input[name="addNode"], input input[name="addNode"]': function(
+    e,
+    template
+  ) {
+    TemplateVar.set('new_node', e.currentTarget.value);
+  },
+  'click button.dapp-block-button': function(e, template) {
+    let custom_nodes = reactive_custom_nodes.get();
+    let new_ndoe = TemplateVar.get('new_node');
+
+    if (!new_ndoe) return;
+
+    if (!custom_nodes) custom_nodes = [];
+    TemplateVar.set(template, 'sending', true);
+
+    http
+      .ping({
+        url: new_ndoe + '/v1/history/get_actions',
+        method: 'post'
+      })
+      .then(tls => {
+        template.$('input[name="addNode"]').val('');
+        TemplateVar.set(template, 'new_node', '');
+
+        let node = { owner: new_ndoe, node: new_ndoe, tls: tls };
+        custom_nodes.unshift(node);
+        localStorage.setItem('custom_nodes', JSON.stringify(custom_nodes));
+        reactive_custom_nodes.set(custom_nodes);
+        TemplateVar.set(template, 'sending', false);
+      });
   }
 });
